a633a0fa144817c8dac8fcaeafb7c5548fa0093f
Sri Aurobindo Munagala <koolschoolfan@gmail.com>
2016-01-26 19:23:02 +0530

    Updated a lot of things

--- Sample_GL3_2D.cpp
+++ Sample_GL3_2D.cpp
@@ -2,11 +2,14 @@
 #include <cmath>
 #include <fstream>
 #include <vector>
-
+#include <string>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+//#include <random>
 
 #define airResistance 0.985
+#define gravity 0.01
+#define NUMBER_OF_LEVELS 4
 #define GLM_FORCE_RADIANS
 #include <glm/glm.hpp>
 #include <glm/gtx/transform.hpp>
@@ -14,27 +17,29 @@
 using namespace std;
 
 double xpos, ypos, xposNew, yposNew;
-int platformNumber = 4, obstacleNumber;
+int isKeyboard = 0; //Set to 1 to use keyboard
+int platformNumber = 4, obstacleNumber = 0;
 double obstacleData[3*15];
 double targetX = 1, targetY = 1;
-double platformData[15*4] = {-3,-3,6,0.5, 3,-2,6,0.5, 4,6,5,0.5, -7,1.5,2,0.5};
-double obstacle[10*3];
+double platformData[15*4]; //= {-3,-3,6,0.5, 3,-2,6,0.5, 4,6,5,0.5, -7,1.5,2,0.5};
+//double obstacle[10*3];
 float canX = -14; float canY = -7; float canR = 0.4;
 int is_ball=0, level=1; //is_ball == 1 if there's a ball in the air
+char levelString[] = "1.txt";
 
-class levelData{
-public:
-  int platformNumber, obstacleNumber;
-  double platformData[4*15], obstacleData[3*15];
-  double targetX, targetY;
 
-}levels[10];
 
-void levelGen(){
-  ifstream fin;
-  fin.open("1.txt");
-
+int random(int min, int max) //range : [min, max)
+{
+   static bool first = true;
+   if ( first )
+   {
+      srand(time(NULL)); //seeding for the first time only!
+      first = false;
+   }
+   return min + rand() % (max - min);
 }
+
 
 struct VAO {
     GLuint VertexArrayID;
@@ -222,46 +227,9 @@ void draw3DObject (struct VAO* vao)
     glDrawArrays(vao->PrimitiveMode, 0, vao->NumVertices); // Starting from vertex 0; 3 vertices total -> 1 triangle
 }
 
-/**************************
- * Customizable functions *
- **************************/
-
-float triangle_rot_dir = 1;
-float rectangle_rot_dir = 1;
-bool triangle_rot_status = true;
-bool rectangle_rot_status = true;
 
 /* Executed when a regular key is pressed/released/held-down */
 /* Prefered for Keyboard events */
-void keyboard (GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-     // Function is called first on GLFW_PRESS.
-
-    if (action == GLFW_RELEASE) {
-        switch (key) {
-            case GLFW_KEY_C:
-                rectangle_rot_status = !rectangle_rot_status;
-                break;
-            case GLFW_KEY_P:
-                triangle_rot_status = !triangle_rot_status;
-                break;
-            case GLFW_KEY_X:
-                // do something ..
-                break;
-            default:
-                break;
-        }
-    }
-    else if (action == GLFW_PRESS) {
-        switch (key) {
-            case GLFW_KEY_ESCAPE:
-                quit(window);
-                break;
-            default:
-                break;
-        }
-    }
-}
 
 /* Executed for character input (like in text boxes) */
 void keyboardChar (GLFWwindow* window, unsigned int key)
@@ -279,7 +247,7 @@ void keyboardChar (GLFWwindow* window, unsigned int key)
 /* Executed when a mouse button is pressed/released */
 
 
-
+GLfloat fov = 2.498f;
 /* Executed when window is resized to 'width' and 'height' */
 /* Modify the bounds of the screen here in glm::ortho or Field of View in glm::Perspective */
 void reshapeWindow (GLFWwindow* window, int width, int height)
@@ -289,7 +257,7 @@ void reshapeWindow (GLFWwindow* window, int width, int height)
      is different from WindowSize */
     glfwGetFramebufferSize(window, &fbwidth, &fbheight);
 
-	GLfloat fov = 90.0f;
+
 
 	// sets the viewport of openGL renderer
 	glViewport (0, 0, (GLsizei) fbwidth, (GLsizei) fbheight);
@@ -300,10 +268,11 @@ void reshapeWindow (GLFWwindow* window, int width, int height)
 	   gluPerspective (fov, (GLfloat) fbwidth / (GLfloat) fbheight, 0.1, 500.0); */
 	// Store the projection matrix in a variable for future use
     // Perspective projection for 3D views
-    // Matrices.projection = glm::perspective (fov, (GLfloat) fbwidth / (GLfloat) fbheight, 0.1f, 500.0f);
+    //ov=1.0f;
+     Matrices.projection = glm::perspective (fov, (GLfloat) fbwidth / (GLfloat) fbheight, 0.1f, 500.0f);
 
     // Ortho projection for 2D views
-    Matrices.projection = glm::ortho(-16.0f, 16.0f, -9.0f, 9.0f, 0.1f, 500.0f);
+    //Matrices.projection = glm::ortho(-16.0f, 16.0f, -9.0f, 9.0f, 0.1f, 500.0f);
 }
 
 VAO *triangle, *rectangle, *circle;
@@ -331,7 +300,7 @@ void createTriangle ()
 }
 
 
-VAO* createCircle(float r)
+VAO* createCircle(float r, float col1 = 1, float col2 = 0.843, float col3 = 0)
   {
     float rad = r;
     //GLfloat vertex_buffer_data[3*3] = {1, 1, 0, -1, 1, 0, -1, -1, 0,};
@@ -351,9 +320,9 @@ VAO* createCircle(float r)
 
      GLfloat color_buffer_data [3*1000];
      for(i=0; i<1000; i+=3){
-      color_buffer_data[i] = 1;
-      color_buffer_data[i+1] = 0.843;
-      color_buffer_data[i+2] = 0;
+      color_buffer_data[i] = col1;
+      color_buffer_data[i+1] = col2;
+      color_buffer_data[i+2] = col3;
     }
 
     // create3DObject creates and returns a handle to a VAO that can be used later
@@ -442,12 +411,10 @@ class map{
     }
 }World;
 
-//class obj;
-//void checkCollision(double , double , double , double, obj );
 
 class obj{
 public:
-  int is_circle;
+  int is_circle, isPhysics;
   double xPos, yPos, width, height, radius;
   double xVel, yVel, xAcc, yAcc;
   VAO* toDraw;
@@ -459,7 +426,11 @@ public:
   }
   void objInit(double xPosNew,double yPosNew,double radiusNew){
     xPos = xPosNew; yPos = yPosNew; radius = radiusNew;
-    toDraw = createCircle(radius);
+    toDraw = createCircle(radius); isPhysics = 0;
+    update();
+  }
+  void setColor(double col1, double col2, double col3){
+    toDraw = createCircle(radius, col1, col2, col3);
     update();
   }
   void reset(double xPosNew, double yPosNew){
@@ -472,6 +443,7 @@ public:
     xVel += xAcc; yVel += yAcc;
     xPos += xVel; yPos += yVel;
     //if(xVel > 0.4)xVel=0.4; if(yVel>0.4)yVel=0.4;
+    if(isPhysics==1) updatePhysics();
     Matrices.model = glm::mat4(1.0f);
     glm::mat4 translate = glm::translate (glm::vec3(xPos, yPos, 0));        // glTranslatef
     glm::mat4 rotate = glm::rotate((float)(0), glm::vec3(0,0,1));
@@ -482,43 +454,56 @@ public:
   }
   void updatePhysics(){
     xVel *= airResistance; yVel *= airResistance;
+    yAcc = -gravity;
   }
 
-}cannonball, cannon, targetA, wall[4], platform[10];
+}cannonball, cannon, targetA, targetInner[4], wall[4], platform[10], obstacle[10];
+
+
 
 void checkCollision(obj &A, obj &B){ //B is a circle, A is a rectangle
   double xRect = A.xPos; double yRect = A.yPos; double width = A.width; double height = A.height;
   double x = B.xPos; double y = B.yPos; double yVel = B.yVel; double xVel = B.xVel;
   //cout<<x<<" "<<y<<" "<<yVel<<" "<<xVel<<endl;
-  if(x > xRect && x < xRect+width && y > yRect+height && y+yVel-canR<yRect+height){
-    B.yPos=yRect+height+canR;
+  if(x > xRect && x < xRect+width && y > yRect+height && y+yVel-B.radius<yRect+height){
+    B.yPos=yRect+height+B.radius;
     //cout<<B.yPos<<" "<<cannonball.yPos<<endl;
     B.yVel*=-0.9;
     B.xVel*=0.98;
   }
-  else if(x > xRect && x < xRect+width && y < yRect&& y+yVel+canR>yRect){
-    B.yPos=yRect-canR;
+  else if(x > xRect && x < xRect+width && y < yRect&& y+yVel+B.radius>yRect){
+    B.yPos=yRect-B.radius;
     B.yVel*=-0.9;
   }
-  else if(y > yRect && y < yRect+height && x > xRect+width && x+xVel-canR<xRect+width){
-    B.xPos = xRect+width+canR;
+  else if(y > yRect && y < yRect+height && x > xRect+width && x+xVel-B.radius<xRect+width){
+    B.xPos = xRect+width+B.radius;
     B.xVel*=-1;
   }
-  else if(y > yRect && y < yRect+height && x < xRect && x+xVel+canR>xRect){
-    B.xPos = xRect-canR;
+  else if(y > yRect && y < yRect+height && x < xRect && x+xVel+B.radius>xRect){
+    B.xPos = xRect-B.radius;
     B.xVel*=-1;
   }
 }
 
-int checkColloisionCircle(obj &firstBall, obj &secondBall){ //Circle //For now, B is target
+int checkCollisionCircle(obj &firstBall, obj &secondBall){ //Circle //For now, B is target
   if (firstBall.xPos + firstBall.radius + secondBall.radius > secondBall.xPos
-&& firstBall.xPos < secondBall.xPos + firstBall.radius + secondBall.radius
-&& firstBall.yPos + firstBall.radius + secondBall.radius > secondBall.yPos
-&& firstBall.yPos < secondBall.yPos + firstBall.radius + secondBall.radius)
-{
-  //AABBs are overlapping
-  cout<<"YAY!"<<endl;
-}
+    && firstBall.xPos < secondBall.xPos + firstBall.radius + secondBall.radius
+    && firstBall.yPos + firstBall.radius + secondBall.radius > secondBall.yPos
+    && firstBall.yPos < secondBall.yPos + firstBall.radius + secondBall.radius)
+  {
+    //AABBs are overlapping
+    if(secondBall.xVel < firstBall.xVel)
+    secondBall.xVel += firstBall.xVel*0.5, firstBall.xVel /= 2;
+    else
+    firstBall.xVel += secondBall.xVel*0.5, secondBall.xVel /= 2;
+
+    if(secondBall.yVel < firstBall.yVel)
+    secondBall.yVel += firstBall.yVel*0.5, firstBall.yVel /= 2;
+    else
+    firstBall.yVel += secondBall.yVel*0.5, secondBall.yVel/=2;
+    return 1;
+  }
+  return 0;
 }
 
 void makewalls(){
@@ -526,10 +511,8 @@ void makewalls(){
   wall[1].objInit(-16, -9, 0.2, 18);
   wall[2].objInit(-16, 8.8, 32, 0.2);
   wall[3].objInit(15.8, -9, 0.2, 18);
-
-  targetA.objInit(targetX, targetY, 0.8);
-
   for(int i=0; i<platformNumber; i++){
+    //cout<<platformData[i]<<endl;
     platform[i].objInit(platformData[4*i], platformData[4*i+1], platformData[4*i+2], platformData[4*i+3]);
     //if(i==1)platform[i].yVel=0.02;
   }
@@ -537,38 +520,84 @@ void makewalls(){
   for(int i=0; i<platformNumber; i++)platform[i].update();
 }
 
+void keyboard (GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+
+  if (action == GLFW_RELEASE) {
+    switch (key) {
+      case GLFW_KEY_SPACE:
+        cannonball.isPhysics = 1;
+        double a = sqrt((xposNew-canX)*(xposNew-canX) + (yposNew-canY)*(yposNew-canY));
+        cannonball.reset(canX + (xposNew-canX)*0/a , canY + (yposNew-canY)*0/a);
+        cannonball.xVel = (xposNew-canX)*1/20 ; cannonball.yVel = (yposNew-canY)*1/20;
+      break;
+    }
+  }
+  else if (action == GLFW_PRESS) {
+    switch (key) {
+      case GLFW_KEY_ESCAPE:
+      quit(window);
+      break;
+      default:
+      break;
+    }
+  }
+}
+
+void levelGen(){
+  ifstream fin;
+  levelString[0] = char(level)+'0';
+  //cout<<levelString<<endl;
+  fin.open(levelString);
+  fin>>platformNumber; //cout<<platformNumber;
+  for(int i=0; i<4*platformNumber; i++)fin>>platformData[i];
+  fin>>targetX;
+  fin>>targetY;
+  makewalls();
+
+  targetA.objInit(targetX, targetY, 0.8); targetA.reset(targetX, targetY);
+  targetInner[0].objInit(targetX, targetY, 0.6); targetInner[0].setColor(1, 1, 0.878);
+  targetInner[1].objInit(targetX, targetY, 0.5); targetInner[1].setColor(0.275, 0.510, 0.706);
+  targetInner[2].objInit(targetX, targetY, 0.4); targetInner[2].setColor(0.902, 0.902, 0.980);
+  targetInner[3].objInit(targetX, targetY, 0.3); targetInner[3].setColor(0.863, 0.078, 0.235);
+
+}
+float panX=0, panY=0; int panState=0;
+
+//Divanshu, set keyboard controls as specified in the requirements.
+//Press alt+~ to get to the other atom tab, I have 1.txt and 2.txt, levels - Modify and add more x.txt's.
+//NUMBER_OF_LEVELS - #Defined. Change to modify the number of levels.
+
 void mouseButton (GLFWwindow* window, int button, int action, int mods)
 {
     switch (button) {
         case GLFW_MOUSE_BUTTON_LEFT:
 
-          if(is_ball==0){
-              is_ball = 1;
-
-              //ball.throw();
-            }
-
             if (action == GLFW_RELEASE){
+                cannonball.isPhysics = 1;
                 double a = sqrt((xposNew-canX)*(xposNew-canX) + (yposNew-canY)*(yposNew-canY));
                 cannonball.reset(canX + (xposNew-canX)*0/a , canY + (yposNew-canY)*0/a);
                 cannonball.xVel = (xposNew-canX)*1/20 ; cannonball.yVel = (yposNew-canY)*1/20;
-                //cout<<cannonball.xVel<<" "<<cannonball.yVel<<" "<<cannonball.yVel/cannonball.xVel<<endl;
-                cannonball.yAcc = -0.01;
-                triangle_rot_dir *= -1;
               }
             break;
         case GLFW_MOUSE_BUTTON_RIGHT:
-            if (action == GLFW_RELEASE) {
-                rectangle_rot_dir *= -1;
-            }
+            panState = 1;
+            if (action == GLFW_RELEASE)panState = 0;
             break;
         default:
             break;
     }
 }
 
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+  fov -= yoffset/200;
+  if(fov>2.498f)fov=2.498f;
+}
+
 /* Render the scene with openGL */
 /* Edit this function according to your assignment */
+
 void draw ()
 {
   // clear the color and depth in the frame buffer
@@ -587,19 +616,16 @@ void draw ()
 
   // Compute Camera matrix (view)
   // Matrices.view = glm::lookAt( eye, target, up ); // Rotating Camera for 3D
-  //  Don't change unless you are sure!!
-  Matrices.view = glm::lookAt(glm::vec3(0,0,3), glm::vec3(0,0,0), glm::vec3(0,1,0)); // Fixed camera for 2D (ortho) in XY plane
 
+  //  Don't change unless you are sure!!
+  if(panState==1)panX+=xposNew/100, panY+=yposNew/100;
+  Matrices.view = glm::lookAt(glm::vec3(panX,panY,3), glm::vec3(panX,panY,0), glm::vec3(0,1,0)); // Fixed camera for 2D (ortho) in XY plane
+  int fbwidth=1280, fbheight=720;
+  Matrices.projection = glm::perspective (fov, (GLfloat) fbwidth / (GLfloat) fbheight, 0.1f, 500.0f);
   // Compute ViewProject matrix as view/camera might not be changed for this frame (basic scenario)
   //  Don't change unless you are sure!!
   VP = Matrices.projection * Matrices.view;
 
-  // Send our transformation to the currently bound shader, in the "MVP" uniform
-  // For each model you render, since the MVP will be different (at least the M part)
-  //  Don't change unless you are sure!!
-  	// MVP = Projection * View * Model
-
-  // Load identity to model matrix
   Matrices.model = glm::mat4(1.0f);
 
   // Pop matrix to undo transformations till last push matrix instead of recomputing model matrix
@@ -609,25 +635,38 @@ void draw ()
 
   //draw walls
   for(int i=0; i<4; i++){wall[i].update(); checkCollision(wall[i], cannonball);}
+  for(int i=0; i<4; i++){wall[i].update(); checkCollision(wall[i], targetA);}
+  for(int i=0; i<platformNumber; i++){platform[i].update(); checkCollision(platform[i], targetA);}
+  for(int i=0; i<4; i++)for(int j=0; j<obstacleNumber; j++)checkCollision(wall[i], obstacle[j]);
+  for(int i=0 ; i<platformNumber; i++)for(int j=0; j<obstacleNumber; j++)checkCollision(platform[i], obstacle[j]);
+  for(int j=0; j<obstacleNumber; j++)if(checkCollisionCircle(cannonball, obstacle[j]));
   for(int i=0; i<platformNumber; i++){platform[i].update(); checkCollision(platform[i], cannonball);}
+  for(int i=0; i<obstacleNumber; i++)for(int j=0; j<i; j++)checkCollisionCircle(obstacle[i], obstacle[j]);
+
+  //for(int i=0; i<obstacleNumber; i++)for(int j=0; j<obstacleNumber; j++)checkCollisionCircle(obstacle[i], obstacle[j]);
   targetA.update();
+  for(int i=0; i<4; i++)targetInner[i].update();
 
   //Draw cannon
   cannon.update();
-
   //Draw cannonball
   cannonball.update();
-  cannonball.updatePhysics();
-  checkColloisionCircle(cannonball, targetA);
-  // Increment angles
-  float increments = 1;
 
-  //camera_rotation_angle++; // Simulating camera rotation
-  triangle_rotation =  triangle_rotation + increments*triangle_rot_dir*triangle_rot_status;
-  rectangle_rotation = rectangle_rotation + 10*increments*rectangle_rot_dir*rectangle_rot_status;
+  for(int j=0; j<obstacleNumber; j++)
+  obstacle[j].update();
 
-  //Translation
-  translateY += 0.01 ;
+  if(checkCollisionCircle(cannonball, targetA)){
+    cannonball.isPhysics = 0; cannonball.reset(canX , canY);
+    level++; if(level > NUMBER_OF_LEVELS)level = 1;
+    levelGen();
+
+    if (targetA.xVel>1) {
+      targetA.xVel = 1;
+    }
+    if (targetA.yVel>1) {
+      targetA.yVel = 1  ;
+    }
+  }
   glFlush();
 }
 
@@ -673,6 +712,7 @@ GLFWwindow* initGLFW (int width, int height)
     /* Register function to handle keyboard input */
     glfwSetKeyCallback(window, keyboard);      // general keyboard input
     glfwSetCharCallback(window, keyboardChar);  // simpler specific character handling
+    glfwSetScrollCallback(window, scroll_callback);
 
     /* Register function to handle mouse click */
     glfwSetMouseButtonCallback(window, mouseButton);  // mouse button clicks
@@ -685,24 +725,19 @@ GLFWwindow* initGLFW (int width, int height)
 void initGL (GLFWwindow* window, int width, int height)
 {
     /* Objects should be created before any other gl function and shaders */
-	// Create the models
-	createTriangle (); // Generate the VAO, VBOs, vertices data & copy into the array buffer
-	//base.createRectangle (-4, -2, 1, 1);
-  //cannon.createCircle (0.2);
   World.mapInit();
   makewalls();
-  cannonball.objInit(77, 77, canR);
+  cannonball.objInit(77, 77, canR); cannonball.setColor(1, 0.7, 0) ;cannonball.isPhysics = 1;
+  for(int j=0; j<obstacleNumber; j++)obstacle[j].objInit(random(-6, 6), random(-6, 6), canR); //obstacle.isPhysics = 1;
   cannon.objInit(canX, canY, 1.4);
-	//ball.createCircle (1);
+
+
 	// Create and compile our GLSL program from the shaders
 	programID = LoadShaders( "Sample_GL.vert", "Sample_GL.frag" );
 	// Get a handle for our "MVP" uniform
 	Matrices.MatrixID = glGetUniformLocation(programID, "MVP");
-
-
 	reshapeWindow (window, width, height);
-
-    // Background color of the scene
+  // Background color of the scene
 	glClearColor (0.3f, 0.3f, 0.3f, 0.0f); // R, G, B, A
 	glClearDepth (1.0f);
 
@@ -721,8 +756,8 @@ int main (int argc, char** argv)
 	int height = 720;
 
     GLFWwindow* window = initGLFW(width, height);
-
-	initGL (window, width, height);
+    levelGen();
+	   initGL (window, width, height);
 
     double last_update_time = glfwGetTime(), current_time;
 
@@ -742,7 +777,7 @@ int main (int argc, char** argv)
             glfwSwapBuffers(window);
 
             glfwGetCursorPos(window, &xpos, &ypos);
-            xposNew = (xpos-(1280/2))*32/1280; yposNew = (-ypos+(720/2))*18/720;
+            xposNew = (xpos-(1280/2))*32*fov/(2.498*1280); yposNew = (-ypos+(720/2))*fov*18/(2.498*720);
             //cout<<ypos<<" "<<xposNew<<" "<<yposNew<<" "<<(yposNew+7)/(xposNew+14)<<endl;
             // Poll for Keyboard and mouse events
             glfwPollEvents();
